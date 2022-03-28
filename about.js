370ca845a1ce85a36a25a425e16476832c96fab5
Spencer Brazell <sa.brazell11@gmail.com>
2022-03-27 21:17:15 -0600

    about.js working

---
+++ about.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	console.log('form submit');
+}
+
+
+let form = document.querySelector('form#contact');
+
+// form.addEventListener('submit', handleSubmit);
+
+form.addEventListener('submit', () => {
+	alert('Thank you for your submission!')
+});
