4ca2ab9037e2770e25eabcc6b5d69f68a0d2265a
Spencer Brazell <sa.brazell11@gmail.com>
2022-03-27 23:30:22 -0600

    profile.js working

---
+++ profile.js
@@ -0,0 +1,19 @@
+
+const button = document.querySelector('button#color');
+button.addEventListener('click', () => {
+    alert('Green');
+  });
+
+
+
+const button2 = document.querySelector('button#place');
+button2.addEventListener('click', () => {
+    alert('Palm Beach Florida');
+  });
+
+
+
+const button3 = document.querySelector('button#ritual');
+button3.addEventListener('click', () => {
+    alert('Watching one Good Good Golf video before I start work');
+  });
